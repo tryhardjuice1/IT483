287f24028bdbf0dfad2bece43672a62f8aa1126c
tryhardjuice1 <44074646+tryhardjuice1@users.noreply.github.com>
2019-11-18 10:16:29 -0600

    Add files via upload

---
+++ FarenheitToCelsius.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter farenheit temperature");
+            string input = Console.ReadLine();
+            decimal farenheit = Convert.ToDecimal(input);
+
+            decimal celsius = (farenheit - 32) * 5 / 9;
+
+            Console.WriteLine(farenheit + " degrees is " + celsius + " degrees celsius");
+        }
+    }
+}
